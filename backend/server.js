cac1df4cf63e73b569b619a16fc5677f665d92f6
Priyanshu <163508521+impriyanshhu@users.noreply.github.com>
2025-12-26 12:35:04 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
-import fetch from 'node-fetch'
 
 dotenv.config()
 
 const app = express()
-app.use(cors())
+app.use(
+  cors({
+    origin: "https://newz-web-coral.vercel.app",
+    credentials: true,
+  })
+);
+
 
 const PORT = process.env.PORT || 5000
 const api = process.env.API_KEY
